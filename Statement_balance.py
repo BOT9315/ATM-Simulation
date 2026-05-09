efe4e08d1fe0055005bd695c51dffa3fc30d8cd9
Ankush <akrk053@gmail.com>
2026-05-09 10:12:14 +0530

    Format statement function for better readability

--- Statement_balance.py
+++ Statement_balance.py
@@ -4,6 +4,7 @@ def statement():
     total_deposit = sum(np.deposits)
     total_withdraw = sum(np.withdrawals)
     current_balance = total_deposit - total_withdraw
+
     
     print(f"\n--- Account Statement/History ---")
     print(f"Total Deposited in your account:  {total_deposit}")
